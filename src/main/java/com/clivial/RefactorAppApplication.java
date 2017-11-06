29c957e7f2c2049717d7f6db75007bdaad0f79c5
刘君 <liujun@xianjinxia.com>
2017-11-06 20:19:53 +0800

    first commit project

---
+++ src/main/java/com/clivial/RefactorAppApplication.java
@@ -0,0 +1,12 @@
+package com.clivial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorAppApplication.class, args);
+	}
+}
